118d76e53deb25274774409210b04af488ea1157
Ilya Kholenkov <ilya@os76.ru>
2017-05-15 18:58:49 +0300

    Comments fix.

--- src/Validator.js
+++ src/Validator.js
@@ -209,7 +209,7 @@ export class Validator {
     /**
      * @param {String} fieldName
      * @param {Array|Object|String} rule
-     * Array: ['Rule1', Rule2, ...]
+     * Array: ['Rule1', ['Rule2', {param: paramVal, ...}], Rule3, ...]
      * Object: Rule
      * Object: {'Rule1': {param: paramVal, ...}, 'Rule2': null, ...}
      * String: 'Rule'
